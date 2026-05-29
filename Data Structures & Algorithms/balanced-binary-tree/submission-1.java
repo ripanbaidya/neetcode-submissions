e1afcc31f421f30afd82fcc9add31d25104bb29e
Ripan Baidya <116362939+ripanbaidya@users.noreply.github.com>
2026-05-29 22:20:52 +0530

    Add: balanced-binary-tree - submission-1

---
+++ Data Structures & Algorithms/balanced-binary-tree/submission-1.java
@@ -0,0 +1,24 @@
+class Solution {
+  public boolean isBalanced(TreeNode root) {
+    if (root == null)
+        return true;
+    
+    int left = height(root.left);
+    int right = height(root.right);
+    
+    if (Math.abs(left - right) > 1)
+        return false;
+    
+    return isBalanced(root.left) && isBalanced(root.right);
+  }
+
+  private int height(TreeNode root) {
+    if (root == null)
+      return 0;
+
+    int leftHeight = height(root.left); 
+    int rightHeight = height(root.right);
+
+    return 1 + Math.max(leftHeight, rightHeight);
+  }
+}
